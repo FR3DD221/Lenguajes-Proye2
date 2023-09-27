d6927e38990fdf8ca0d5cb4155e9f7d3f8de4ac5
Fredd Badilla Víquez <freddalessandro2876@gmail.com>
2023-09-26 21:49:41 -0600

    commit inicial
    Se inicializan las funciones claven con para si poder comenzar el desarrollo de este proyecto

---
+++ Programa/main.hs
@@ -0,0 +1,46 @@
+import System.IO
+import System.Directory (doesPathExist)
+
+--C://Users//fredd//OneDrive//Documentos//TEC/LENGUAJES//PP2_Fredd_Randall//Programa//
+
+rutaExiste ruta = doesPathExist ruta
+
+leerArchivo ruta = do
+    existe <- rutaExiste ruta
+    if existe then do 
+        contenido <- readFile ruta
+        let lineas = lines contenido
+        return lineas
+    else do
+        putStrLn("\nERROR -> La ruta ingresada es invalida")
+        return []
+
+añadirEnArchivo ruta nuevoCont = do
+    existe <- rutaExiste ruta
+    if existe then do
+        appendFile ruta nuevoCont
+        putStrLn("\nAVISO -> Informacion agregada con exito")
+    else 
+        putStrLn("\nERROR -> La ruta ingresada es invalida")
+
+sobreEscribirEnArchivo ruta nuevoCont = do
+    existe <- rutaExiste ruta
+    if existe then do
+        writeFile ruta nuevoCont
+        putStrLn("\nAVISO -> Informacion sobreescrita con exito")
+    else 
+        putStrLn("\nERROR -> La ruta ingresada es invalida")
+
+split [] token = [token]
+split (x:xs) token
+  | x == ',' = token : split xs "" 
+  | otherwise = split xs (token ++ [x])
+
+
+
+main :: IO ()
+main = do
+    let fuap = "a,b,c,d"
+    let result = split fuap ""
+
+    putStrLn (show result)
